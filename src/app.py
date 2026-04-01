2430899dd9aa0094c619fdca5810b597bc389d41
huskygut <youngbucks603@gmail.com>
2026-03-31 21:08:23 -0500

    Add src folder with main application files

--- src/app.py
+++ src/app.py
@@ -1,4 +1,11 @@
-# Empty file
-# Dependencies:
-# Pillow
-# heatshrink2
+import tkinter as tk
+
+from gui import FlipperMomentumGifMakerApp
+
+def main() -> None:
+    root = tk.Tk()
+    FlipperMomentumGifMakerApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
