08f144edbcc76b9909c3583bb82e464316cccc8f
huskygut <youngbucks603@gmail.com>
2026-03-31 21:09:09 -0500

    Add FlipperMomentumGifMakerApp GUI class to manifest.py

--- src/manifest.py
+++ src/manifest.py
@@ -1,2 +1,7 @@
-# Flipper Momentum GIF Maker App
-# Define the FlipperMomentumGifMakerApp GUI class here
+class FlipperMomentumGifMakerApp:
+    # Implementation of the FlipperMomentumGifMakerApp GUI class
+    def __init__(self):
+        pass
+
+    def run(self):
+        print("Running Flipper Momentum GIF Maker App")
